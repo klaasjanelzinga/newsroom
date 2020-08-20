9742e4ce0f43e1aabc6572ec871fade94b25d24a
KlaasJan <klaasjanelzinga@me.com>
2020-08-20 09:28:22 +0200

    flakes

--- core_lib/tests/test_user_profile.py
+++ core_lib/tests/test_user_profile.py
@@ -2,5 +2,7 @@ from core_lib.user_profile import UserProfile
 
 
 def test_create():
-    user_profile = UserProfile(given_name="test", email="", family_name="", avatar_url="", is_approved=False)
+    user_profile = UserProfile(
+        given_name="test", email="", family_name="", avatar_url="", is_approved=False
+    )
     assert user_profile is not None
